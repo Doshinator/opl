929dd5d908217236bf47d72dd4a5a4ee2cbf2c3a
Rushabh Doshi <doshi@mac.mynetworksettings.com>
2026-01-30 20:59:06 -0500

    Exp, Add, Mult

--- main.cpp
+++ main.cpp
@@ -1,6 +1,48 @@
 #include <print>
 
+class Expression {};
+
+class Num : public Expression {
+    private:
+        int n;
+    public:
+        Num(int n) { this->n = n; }
+};
+
+class Add : public Expression {
+    private:
+        Expression *lhs;
+        Expression *rhs;
+    public:
+        Add(Expression *lhs, Expression *rhs) {
+            this->lhs = lhs;
+            this->rhs = rhs;
+        }
+        ~Add() {
+            delete lhs;
+            delete rhs;
+        }
+};
+
+class Multiply : public Expression {
+    private:
+        Expression *lhs;
+        Expression *rhs;
+    public:
+        Multiply(Expression *lhs, Expression *rhs) {
+            this->lhs = lhs;
+            this->rhs = rhs;
+        }
+        ~Multiply() {
+            delete lhs;
+            delete rhs;
+        }
+};
+
+
 int main() {
-    std::println("Hello");
+    std::println("---- Start -----");
+    
+    std::println("---- End -----");
     return 0;
 }
