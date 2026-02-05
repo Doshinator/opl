f7589e22d50818049171bcb39d9aa460deeb01df
Rushabh Doshi <doshi@mac.mynetworksettings.com>
2026-02-04 22:14:47 -0500

    claering out the fn. attempting this tomorrow

--- src/s_expression.rs
+++ src/s_expression.rs
@@ -1,42 +1,19 @@
 use std::{iter::Peekable, str::Chars};
 
 pub enum SExpr {
-    SESym(String),
-    SExprNum(i64),
-    SExprList(Vec<Box<SExpr>>),
+    Num(i64),
+    Sym(String),
+    List(Vec<SExpr>),
 }
 
 // "(+ 2 (* 3 4))" -> list[+, 2, list[*, 3, 4]]
+/*
+    case white space - skip
+    case number
+    case non-number
+    case (
+ */
+
 pub fn reader(str: &str) -> SExpr {
-    // case number
-    // case non-number
-    // case (
-
-    for c in str.chars() {
-        print!("{}", c);
-    }
-    // parse
-    // finish impl of reader to turn string into SExpr
-    SExpr::SExprNum(5)
-}
-
-pub fn read_expr(char: &mut Peekable<Chars>) -> SExpr {
-    /*
-    skip whitespace
-
-    if next char == '('
-        consume (
-        let elements = []
-        while next char != ')'
-            elements.push(read_expr(char))
-        consume ')'
-        return List(elements)
-    else if next char is digit or '-'
-        read number
-        return Num(n)
-    
-    else
-        read symbol
-        return Symbol(name)
-    */
+    todo!()
 }
