4e009d1d63fb7fadf52788b4d79321591cbacda0
Rushabh Doshi <doshi@mac.mynetworksettings.com>
2026-02-07 23:57:01 -0500

    added additional complex test

--- tests/s_expression_test.rs
+++ tests/s_expression_test.rs
@@ -1,12 +1,11 @@
 use opl::s_expression::{SExpr, reader};
 
 #[test]
-fn buid_list() {
+fn read_number() {
     // Arrange
-    let s = "( + 2 3 )";
-    let expected = SExpr::List(
-        Box::new(SExpr::Sym("+").try_into())
-    );
+    let s = "2";
+    let expected = SExpr::Num(2);
+
     // Act
     let actual = reader(s);
     
@@ -14,5 +13,63 @@ fn buid_list() {
     assert_eq!(
         expected,
         actual 
+    );
+}
+
+#[test]
+fn read_symbol() {
+    // Arrange
+    let s = "+";
+    let expected = SExpr::Sym("+".into());
+
+    // Act
+    let actual = reader(s);
+    
+    // Assert
+    assert_eq!(
+        expected,
+        actual 
+    );
+}
+
+#[test]
+fn read_list() {
+    // Arrange
+    let s = "(+ 1 2)";
+    let expected = SExpr::List(vec![
+        SExpr::Sym("+".into()),
+        SExpr::Num(1),
+        SExpr::Num(2),
+    ]);
+
+    // Act
+    let actual = reader(s);
+
+    // Arrange
+    assert_eq!(
+        expected,
+        actual,
     )
+}
+
+#[test]
+fn read_nested_list() {
+    // Arrange
+    let s = "(+ 2 (* 3 4))";
+    let expected = SExpr::List(vec![
+        SExpr::Sym("+".into()),
+        SExpr::Num(2),
+        SExpr::List(vec![
+            SExpr::Sym("*".into()),
+            SExpr::Num(3),
+            SExpr::Num(4),
+        ]),
+    ]);
+
+    // Act
+    let actual = reader(s);
+    assert_eq!(
+        actual, 
+        expected
+    );
 }
