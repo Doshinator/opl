b4644614780f4d2b7ff026388fc3b351e064bf3b
Rushabh Doshi <rush3doshi@gmail.com>
2026-02-18 20:32:17 -0500

    adding tests

--- src/context.rs
+++ src/context.rs
@@ -1,4 +1,4 @@
-use crate::Expr;
+use crate::{Expr, Value};
 
 // ex: (+ 1 (if (+ 2 []) 3 4)) -> AppC([+ 1] (If1C (AppC [+ 2] Hole []) 3 4) [])
 #[derive(Debug, PartialEq, Clone)]
@@ -11,11 +11,12 @@ pub enum Context {
     MulL(Box<Context>, Box<Expr>),
     DivL(Box<Context>, Box<Expr>),
     
-    // Arithmatic - Right side
-    AddR(Box<Expr>, Box<Context>),
-    SubR(Box<Expr>, Box<Context>),
-    MulR(Box<Expr>, Box<Context>),
-    DivR(Box<Expr>, Box<Context>),
+    // Arithmatic - Right side 
+    // Value here becaue we have left to right evaluation rules
+    AddR(Value, Box<Context>),
+    SubR(Value, Box<Context>),
+    MulR(Value, Box<Context>),
+    DivR(Value, Box<Context>),
 
     // Comparison - Left side
     LessL(Box<Context>, Box<Expr>),
@@ -25,12 +26,18 @@ pub enum Context {
     EqualL(Box<Context>, Box<Expr>),
 
     // Comaprison - Right side
-    LessR(Box<Expr>, Box<Context>),
-    LessEqR(Box<Expr>, Box<Context>),
-    GreaterR(Box<Expr>, Box<Context>),
-    GreaterEqR(Box<Expr>, Box<Context>),
-    EqualR(Box<Expr>, Box<Context>),
+    LessR(Value, Box<Context>),
+    LessEqR(Value, Box<Context>),
+    GreaterR(Value, Box<Context>),
+    GreaterEqR(Value, Box<Context>),
+    EqualR(Value, Box<Context>),
 
     // If cond can only have context in condition
     If(Box<Context>, Box<Expr>, Box<Expr>),
+}
+
+impl Context {
+    pub fn is_hole(&self) -> bool {
+        matches!(self, Context::Hole)
+    }
 }
--- tests/context_test.rs
+++ tests/context_test.rs
@@ -1,20 +1,98 @@
-use opl::{Context, Expr};
+use opl::{Context, Expr, Value};
 
 #[test]
 fn context_hole() {
-    let c = Context::Hole;
-    assert_eq!(c, Context::Hole);
+    let ctx = Context::Hole;
+    assert!(ctx.is_hole());
 }
 
 #[test]
-fn add_left_context() {
+fn test_add_left_not_hole() {
     // (+ HOLE 5)
-    let c = Context::AddL(
+    let ctx = Context::AddL(
         Box::new(Context::Hole), 
         Box::new(Expr::Num(5))
     );
-    match c {
-        Context::AddL(_, _) => assert!(true),
+    
+    assert!(!ctx.is_hole());
+    match ctx {
+        Context::AddL(l, r) => {
+            assert!(l.is_hole());
+            assert_eq!(*r, Expr::Num(5));
+        },
         _ => panic!("Wrong variant"),
+    }
+}
+
+#[test]
+fn context_add_right_simple() {
+    // (+ 2 HOLE)
+    let ctx = Context::AddR(
+        Value::Num(2),
+        Box::new(Context::Hole)
+    );
+
+    assert!(!ctx.is_hole());
+    match ctx {
+        Context::AddR(l, r) => {
+            assert_eq!(l, Value::Num(2));
+            assert!(r.is_hole());
+        },
+        _ => panic!("Wrong variant"),
+    }
+}
+
+#[test]
+fn nested_context_add_mult() {
+    // (+ 2 (* HOLE 4))
+    let ctx = Context::AddR(
+        Value::Num(2), 
+        Box::new(Context::MulL(
+            Box::new(Context::Hole), 
+            Box::new(Expr::Num(4))
+        ))
+    );
+
+    match ctx {
+        Context::AddR(left_value, inner) => {
+            assert_eq!(left_value, Value::Num(2));
+            
+            match &*inner {
+                Context::MulL(mul_inner, r) => {
+                    assert!(mul_inner.is_hole());
+                    assert_eq!(**r, Expr::Num(4));
+                },
+                _ => panic!("Expected MulL"),
+            }
+        },
+        _ => panic!("Expected MulR"),
+    }
+}
+
+#[test]
+fn context_deeply_nested() {
+    // Represents: (+ 1 (* 2 (- HOLE 3)))
+    // Add 1 to (Multiply 2 by (Subtract HOLE from 3))
+    let ctx = Context::AddR(
+        Value::Num(1),
+        Box::new(Context::MulR(
+            Value::Num(2),
+            Box::new(Context::SubL(
+                Box::new(Context::Hole),
+                Box::new(Expr::Num(3))
+            ))
+        ))
+    );
+    
+    // Just verify it compiles and has right structure
+    match ctx {
+        Context::AddR(_, inner) => match &*inner {
+            Context::MulR(_, inner2) => match &**inner2 {
+                Context::SubL(hole, _) => assert!(hole.is_hole()),
+                _ => panic!("Expected SubL"),
+            },
+            _ => panic!("Expected MulR"),
+        },
+        _ => panic!("Expected AddR"),
     }
 }
