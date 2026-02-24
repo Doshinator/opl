66271ab732b26ce585b65d60b6288c96d717ec81
Rushabh Doshi <rush3doshi@gmail.com>
2026-02-24 18:57:48 -0500

    test suit updated

--- tests/context_test.rs
+++ tests/context_test.rs
@@ -126,3 +126,134 @@ fn test_plug_add_right() {
         Box::new(Expr::Num(5))
     ));
 }
+
+#[test]
+fn test_plug_mult_left() {
+    let ctx = Context::MulL(Box::new(Context::Hole), Box::new(Expr::Num(3)));
+
+    let result = plug(&ctx, Expr::Num(4));
+
+    assert_eq!(result, Expr::Mul(Box::new(Expr::Num(4)), Box::new(Expr::Num(3))));
+}
+
+#[test]
+fn test_plug_mult_right() {
+    let ctx = Context::MulR(Value::Num(5), Box::new(Context::Hole));
+
+    let result = plug(&ctx, Expr::Num(7));
+
+    assert_eq!(result, Expr::Mul(Box::new(Expr::Num(5)), Box::new(Expr::Num(7))));
+}
+
+#[test]
+fn test_plug_nested_context() {
+    // (+ 2 (* HOLE 4))
+    let ctx = Context::AddR(
+        Value::Num(2),
+        Box::new(Context::MulL(
+            Box::new(Context::Hole),
+            Box::new(Expr::Num(4))
+        ))
+    );
+
+    let result = plug(&ctx, Expr::Num(3));
+
+    assert_eq!(
+        result,
+        Expr::Add(
+            Box::new(Expr::Num(2)),
+            Box::new(Expr::Mul(
+                Box::new(Expr::Num(3)),
+                Box::new(Expr::Num(4))
+            ))
+        )
+    );
+}
+
+#[test]
+fn test_plug_less_left() {
+    let ctx = Context::LessL(
+        Box::new(Context::Hole),
+        Box::new(Expr::Num(10))
+    );
+
+    let result = plug(&ctx, Expr::Num(3));
+
+    assert_eq!(
+        result,
+        Expr::Less(
+            Box::new(Expr::Num(3)),
+            Box::new(Expr::Num(10))
+        )
+    );
+}
+
+#[test]
+fn test_plug_less_right() {
+    let ctx = Context::LessR(
+        Value::Num(3),
+        Box::new(Context::Hole)
+    );
+
+    let result = plug(&ctx, Expr::Num(5));
+
+    assert_eq!(
+        result,
+        Expr::Less(
+            Box::new(Expr::Num(3)),
+            Box::new(Expr::Num(5))
+        )
+    );
+}
+
+#[test]
+fn test_plug_if_condition() {
+    // (if HOLE 1 2)
+    let ctx = Context::If(
+        Box::new(Context::Hole),
+        Box::new(Expr::Num(1)),
+        Box::new(Expr::Num(2))
+    );
+
+    let result = plug(&ctx, Expr::Bool(true));
+
+    assert_eq!(
+        result,
+        Expr::If(
+            Box::new(Expr::Bool(true)),
+            Box::new(Expr::Num(1)),
+            Box::new(Expr::Num(2))
+        )
+    );
+}
+
+#[test]
+fn test_plug_deep_recursive() {
+    // (+ 1 (* 2 (- HOLE 3)))
+    let ctx = Context::AddR(
+        Value::Num(1),
+        Box::new(Context::MulR(
+            Value::Num(2),
+            Box::new(Context::SubL(
+                Box::new(Context::Hole),
+                Box::new(Expr::Num(3))
+            ))
+        ))
+    );
+
+    let result = plug(&ctx, Expr::Num(7));
+
+    assert_eq!(
+        result,
+        Expr::Add(
+            Box::new(Expr::Num(1)),
+            Box::new(Expr::Mul(
+                Box::new(Expr::Num(2)),
+                Box::new(Expr::Sub(
+                    Box::new(Expr::Num(7)),
+                    Box::new(Expr::Num(3))
+                ))
+            ))
+        )
+    );
+}
