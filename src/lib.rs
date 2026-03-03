9d1ffb8dc2815630072ef4a20902b35918711184
Rushabh Doshi <rush3doshi@gmail.com>
2026-03-02 21:01:15 -0500

    ck Machine init

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ pub mod expression;
 pub mod s_expression;
 pub mod desugar;
 pub mod context;
+pub mod ck0;
 pub use expression::{Expr, Value, eval};
 pub use s_expression::reader;
 pub use desugar::desugar;
